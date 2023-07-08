802b2ed2c8b6a9b63e426cc5d3bd87dc6de6c4a7
Felipe Casalecchi <36058141+casalecchi@users.noreply.github.com>
2023-07-08 15:23:50 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Vamos conseguir!</h1>
+    return <h1>Acredite em você, vamos conseguir!</h1>
 }
 
 // Aqui indicamos que a função Home, que pode conter vários compenentes
